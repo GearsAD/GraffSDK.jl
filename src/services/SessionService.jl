08f9f99b80a1d7744cc6e3889cdc45ba90106225
GearsAD <sclaassens.ad@gmail.com>
2018-02-19 08:46:47 -0600

    Spike on mocking for unit tests

--- src/services/SessionService.jl
+++ src/services/SessionService.jl
@@ -4,27 +4,27 @@ include("../entities/Session.jl")
 sessionsEndpoint = "api/v0/users/{1}/robots/{2]/sessions}"
 sessionEndpoint = "api/v0/users/{1}/robots/{2}/sessions/{3}"
 
-# """
-#     getRobots(config::SynchronyConfig, auth::AuthResponse, userId::String)::RobotsResponse
-# Gets all robots managed by the specified user.
-# Return: A vector of robots for a given user.
-# """
-# function getRobots(config::SynchronyConfig, auth::AuthResponse, userId::String)::RobotsResponse
-#     url = "$(config.apiEndpoint):$(config.apiPort)/$(format(robotsEndpoint, userId))"
-#     response = get(url; headers = Dict("token" => auth.token))
-#     if(statuscode(response) != 200)
-#         error("Error getting robots, received $(statuscode(response)) with body '$(readstring(response))'.")
-#     else
-#         # Some manual effort done here because it's a vector response.
-#         rawRobots = JSON.parse(readstring(response))
-#         robots = RobotsResponse(Vector{RobotResponse}(), rawRobots["links"])
-#         for robot in rawRobots["robots"]
-#             robot = _unmarshallWithLinks(JSON.json(robot), RobotResponse)
-#             push!(robots.robots, robot)
-#         end
-#         return robots
-#     end
-# end
+"""
+    getSessions(config::SynchronyConfig, auth::AuthResponse, userId::String, robotId::String)::SessionsResponse
+Gets all robots managed by the specified user.
+Return: A vector of robots for a given user.
+"""
+function getRobots(config::SynchronyConfig, auth::AuthResponse, userId::String)::RobotsResponse
+    url = "$(config.apiEndpoint):$(config.apiPort)/$(format(robotsEndpoint, userId))"
+    response = get(url; headers = Dict("token" => auth.token))
+    if(statuscode(response) != 200)
+        error("Error getting robots, received $(statuscode(response)) with body '$(readstring(response))'.")
+    else
+        # Some manual effort done here because it's a vector response.
+        rawRobots = JSON.parse(readstring(response))
+        robots = RobotsResponse(Vector{RobotResponse}(), rawRobots["links"])
+        for robot in rawRobots["robots"]
+            robot = _unmarshallWithLinks(JSON.json(robot), RobotResponse)
+            push!(robots.robots, robot)
+        end
+        return robots
+    end
+end
 
 # """
 #     getRobot(config::SynchronyConfig, auth::AuthResponse, userId::String, robotId::String)::RobotResponse
@@ -53,20 +53,5 @@ function createSession(config::SynchronyConfig, auth::AuthResponse, userId::Stri
         error("Error creating session, received $(statuscode(response)) with body '$(readstring(response))'.")
     else
         return _unmarshallWithLinks(readstring(response), SessionDetailsResponse)
-    end
-end
-
-"""
-    getNodes(config::SynchronyConfig, auth::AuthResponse, userId::String, robotId::String, sessionId::String)::RobotResponse
-Get a specific robot given a user ID and a robot ID.
-Return: The robot for the provided user ID and robot ID.
-"""
-function getRobot(config::SynchronyConfig, auth::AuthResponse, userId::String, robotId::String)::RobotResponse
-    url = "$(config.apiEndpoint):$(config.apiPort)/$(format(robotEndpoint, userId, robotId))"
-    response = get(url; headers = Dict("token" => auth.token))
-    if(statuscode(response) != 200)
-        error("Error getting robot, received $(statuscode(response)) with body '$(readstring(response))'.")
-    else
-        return _unmarshallWithLinks(readstring(response), RobotResponse)
     end
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -4,5 +4,5 @@ using SynchronySDK
 # TESTING
 test = "{\"id\":\"NewUser\",\"name\":\"Bob Zacowski\",\"email\":\"email@email.com\",\"address\":\"N/A\",\"organization\":\"Student\",\"licenseType\":\"Student\",\"billingId\":\"96a3877e-738d-4c92-a880-98b0cbfd8937\",\"createdTimestamp\":\"2018-02-10T09:26:01.328\",\"lastUpdatedTimestamp\":\"2018-02-10T09:26:01.328\",\"links\":{\"robots\":\"/api/v0/users/NewUser/robots\",\"config\":\"/api/v0/users/NewUser/config\",\"self\":\"/api/v0/users/NewUser\"}}"
 j = JSON.parse(test)
-user = _unmarshallUser(test, UserResponse)
+user = _unmarshallWithLinks(test, UserResponse)
 @test JSON.json(user) == JSON.json(user)
