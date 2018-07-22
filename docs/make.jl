f17ea3a96aa42f22f01a51b00bbb35131db05346
GearsAD <sclaassens.ad@gmail.com>
2018-07-22 13:28:17 -0500

    Teting documentation deployment

--- docs/make.jl
+++ docs/make.jl
@@ -23,17 +23,7 @@ makedocs(
 deploydocs(
     repo   = "github.com/GearsAD/SynchronySDK.jl.git",
     target = "build",
-    deps   = nothing,
-    make   = nothing,
+    deps   = Deps.pip("mkdocs", "python-markdown-math"),
     julia  = "0.6",
     osname = "linux"
 )
-
-
-
-
-# deploydocs(
-#     deps   = Deps.pip("mkdocs", "python-markdown-math", "mkdocs-material"),
-#     repo   = "github.com/Affie/Video4Linux.jl.git",
-#     julia  = "release",
-# )
